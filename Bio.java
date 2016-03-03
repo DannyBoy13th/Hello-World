ddf768658f027a9493e003974ab9cfb76cd73f26
DannyBoy13th <danielsolo13th@gmail.com>
2016-03-03 20:50:47 +0200

    Bio Experiment
    Added a bit of info about myself

---
+++ Bio.java
@@ -0,0 +1,18 @@
+public class Bio {
+      public static void main (String [] args){
+        String Name = "Name: Daniel Solo";
+        int Age = 23;
+        String Age1 = "Age: ";
+		String Sex = "Sex: Male";
+        String City = "City: Kiev";
+		String Occupation = "Occupation: Head Analyst";
+		String Hobbies = "Hobbies: videogames, books, movies and comics";		
+        System.out.println(Name);
+        System.out.print(Age1);
+        System.out.println(Age);
+		System.out.println(Sex);
+        System.out.println(City);
+		System.out.println(Occupation);
+		System.out.println(Hobbies);
+    }
+}
